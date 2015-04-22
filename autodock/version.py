a9bbfddef6484451aad8e84b4f7e4f79b1d626ea
James Mills <prologic@shortcircuit.net.au>
2015-04-23 00:34:52 +1000

    Preparing for 0.0.1 release

--- autodock/version.py
+++ autodock/version.py
@@ -7,7 +7,7 @@
 So we only have to maintain version information in one place!
 """
 
-version_info = (0, 0, 1, "dev")  # (major, minor, patch, dev?)
+version_info = (0, 0, 1)  # (major, minor, patch, dev?)
 version = (
     ".".join(map(str, version_info))
     if version_info[-1] != "dev"
